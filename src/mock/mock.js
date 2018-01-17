334fb7b4b39567b6705378d15ee7fbafe0aee1b6
liuxing <liuxing06@baidu.com>
2018-01-17 19:47:19 +0800

    将chart展示的数据移入mock

--- src/mock/mock.js
+++ src/mock/mock.js
@@ -1,154 +1,321 @@
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
-import { LoginUsers, Users } from './data/user';
+import {LoginUsers, Users} from './data/user';
+
 let _Users = Users;
 
 export default {
-  /**
-   * mock bootstrap
-   */
-  bootstrap() {
-    let mock = new MockAdapter(axios);
+    /**
+     * mock bootstrap
+     */
+    bootstrap() {
+        let mock = new MockAdapter(axios);
 
-    // mock success request
-    mock.onGet('/success').reply(200, {
-      msg: 'success'
-    });
+        // mock success request
+        mock.onGet('/success').reply(200, {
+            msg: 'success'
+        });
 
-    // mock error request
-    mock.onGet('/error').reply(500, {
-      msg: 'failure'
-    });
+        // mock error request
+        mock.onGet('/error').reply(500, {
+            msg: 'failure'
+        });
 
-    //登录
-    mock.onPost('/login').reply(config => {
-      let {username, password} = JSON.parse(config.data);
-      return new Promise((resolve, reject) => {
-        let user = null;
-        setTimeout(() => {
-          let hasUser = LoginUsers.some(u => {
-            if (u.username === username && u.password === password) {
-              user = JSON.parse(JSON.stringify(u));
-              user.password = undefined;
-              return true;
-            }
-          });
+        //登录
+        mock.onPost('/login').reply(config => {
+            let {username, password} = JSON.parse(config.data);
+            return new Promise((resolve, reject) => {
+                let user = null;
+                setTimeout(() => {
+                    let hasUser = LoginUsers.some(u => {
+                        if (u.username === username && u.password === password) {
+                            user = JSON.parse(JSON.stringify(u));
+                            user.password = undefined;
+                            return true;
+                        }
+                    });
 
-          if (hasUser) {
-            resolve([200, { code: 200, msg: '请求成功', user }]);
-          } else {
-            resolve([200, { code: 500, msg: '账号或密码错误' }]);
-          }
-        }, 1000);
-      });
-    });
+                    if (hasUser) {
+                        resolve([200, {code: 200, msg: '请求成功', user}]);
+                    } else {
+                        resolve([200, {code: 500, msg: '账号或密码错误'}]);
+                    }
+                }, 1000);
+            });
+        });
 
-    //获取用户列表
-    mock.onGet('/user/list').reply(config => {
-      let {name} = config.params;
-      let mockUsers = _Users.filter(user => {
-        if (name && user.name.indexOf(name) == -1) return false;
-        return true;
-      });
-      return new Promise((resolve, reject) => {
-        setTimeout(() => {
-          resolve([200, {
-            users: mockUsers
-          }]);
-        }, 1000);
-      });
-    });
+        //获取用户列表
+        mock.onGet('/user/list').reply(config => {
+            let {name} = config.params;
+            let mockUsers = _Users.filter(user => {
+                if (name && user.name.indexOf(name) == -1) return false;
+                return true;
+            });
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    resolve([200, {
+                        users: mockUsers
+                    }]);
+                }, 1000);
+            });
+        });
 
-    //获取用户列表（分页）
-    mock.onGet('/user/listpage').reply(config => {
-      let {page, name} = config.params;
-      let mockUsers = _Users.filter(user => {
-        if (name && user.name.indexOf(name) == -1) return false;
-        return true;
-      });
-      let total = mockUsers.length;
-      mockUsers = mockUsers.filter((u, index) => index < 20 * page && index >= 20 * (page - 1));
-      return new Promise((resolve, reject) => {
-        setTimeout(() => {
-          resolve([200, {
-            total: total,
-            users: mockUsers
-          }]);
-        }, 1000);
-      });
-    });
+        //获取用户列表（分页）
+        mock.onGet('/user/listpage').reply(config => {
+            let {page, name} = config.params;
+            let mockUsers = _Users.filter(user => {
+                if (name && user.name.indexOf(name) == -1) return false;
+                return true;
+            });
+            let total = mockUsers.length;
+            mockUsers = mockUsers.filter((u, index) => index < 20 * page && index >= 20 * (page - 1));
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    resolve([200, {
+                        total: total,
+                        users: mockUsers
+                    }]);
+                }, 1000);
+            });
+        });
 
-    //删除用户
-    mock.onGet('/user/remove').reply(config => {
-      let { id } = config.params;
-      _Users = _Users.filter(u => u.id !== id);
-      return new Promise((resolve, reject) => {
-        setTimeout(() => {
-          resolve([200, {
-            code: 200,
-            msg: '删除成功'
-          }]);
-        }, 500);
-      });
-    });
+        //删除用户
+        mock.onGet('/user/remove').reply(config => {
+            let {id} = config.params;
+            _Users = _Users.filter(u => u.id !== id);
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    resolve([200, {
+                        code: 200,
+                        msg: '删除成功'
+                    }]);
+                }, 500);
+            });
+        });
 
-    //批量删除用户
-    mock.onGet('/user/batchremove').reply(config => {
-      let { ids } = config.params;
-      ids = ids.split(',');
-      _Users = _Users.filter(u => !ids.includes(u.id));
-      return new Promise((resolve, reject) => {
-        setTimeout(() => {
-          resolve([200, {
-            code: 200,
-            msg: '删除成功'
-          }]);
-        }, 500);
-      });
-    });
+        //批量删除用户
+        mock.onGet('/user/batchremove').reply(config => {
+            let {ids} = config.params;
+            ids = ids.split(',');
+            _Users = _Users.filter(u => !ids.includes(u.id));
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    resolve([200, {
+                        code: 200,
+                        msg: '删除成功'
+                    }]);
+                }, 500);
+            });
+        });
 
-    //编辑用户
-    mock.onGet('/user/edit').reply(config => {
-      let { id, name, addr, age, birth, sex } = config.params;
-      _Users.some(u => {
-        if (u.id === id) {
-          u.name = name;
-          u.addr = addr;
-          u.age = age;
-          u.birth = birth;
-          u.sex = sex;
-          return true;
-        }
-      });
-      return new Promise((resolve, reject) => {
-        setTimeout(() => {
-          resolve([200, {
-            code: 200,
-            msg: '编辑成功'
-          }]);
-        }, 500);
-      });
-    });
+        //编辑用户
+        mock.onGet('/user/edit').reply(config => {
+            let {id, name, addr, age, birth, sex} = config.params;
+            _Users.some(u => {
+                if (u.id === id) {
+                    u.name = name;
+                    u.addr = addr;
+                    u.age = age;
+                    u.birth = birth;
+                    u.sex = sex;
+                    return true;
+                }
+            });
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    resolve([200, {
+                        code: 200,
+                        msg: '编辑成功'
+                    }]);
+                }, 500);
+            });
+        });
 
-    //新增用户
-    mock.onGet('/user/add').reply(config => {
-      let { name, addr, age, birth, sex } = config.params;
-      _Users.push({
-        name: name,
-        addr: addr,
-        age: age,
-        birth: birth,
-        sex: sex
-      });
-      return new Promise((resolve, reject) => {
-        setTimeout(() => {
-          resolve([200, {
-            code: 200,
-            msg: '新增成功'
-          }]);
-        }, 500);
-      });
-    });
+        //新增用户
+        mock.onGet('/user/add').reply(config => {
+            let {name, addr, age, birth, sex} = config.params;
+            _Users.push({
+                name: name,
+                addr: addr,
+                age: age,
+                birth: birth,
+                sex: sex
+            });
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    resolve([200, {
+                        code: 200,
+                        msg: '新增成功'
+                    }]);
+                }, 500);
+            });
+        });
+        //Column Cahrt
+        mock.onGet(`/chart/column`).reply(config => {
+            return new Promise(((resolve, reject) => {
+                setTimeout(() => {
+                    resolve([200, {
+                        title: {text: 'Column Chart'},
+                        tooltip: {},
+                        xAxis: {
+                            data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+                        },
+                        yAxis: {},
+                        series: [{
+                            name: '销量',
+                            type: 'bar',
+                            data: [5, 20, 36, 10, 10, 20]
+                        }]
+                    }])
+                })
+            }))
+        })
+        //Bar Chart
+        mock.onGet(`/chart/bar`).reply(config => {
+            return new Promise(((resolve, reject) => {
+                setTimeout(() => {
+                    resolve([200,{
+                        title: {
+                            text: 'Bar Chart',
+                            subtext: '数据来自网络'
+                        },
+                        tooltip: {
+                            trigger: 'axis',
+                            axisPointer: {
+                                type: 'shadow'
+                            }
+                        },
+                        legend: {
+                            data: ['2011年', '2012年']
+                        },
+                        grid: {
+                            left: '3%',
+                            right: '4%',
+                            bottom: '3%',
+                            containLabel: true
+                        },
+                        xAxis: {
+                            type: 'value',
+                            boundaryGap: [0, 0.01]
+                        },
+                        yAxis: {
+                            type: 'category',
+                            data: ['巴西', '印尼', '美国', '印度', '中国', '世界人口(万)']
+                        },
+                        series: [
+                            {
+                                name: '2011年',
+                                type: 'bar',
+                                data: [18203, 23489, 29034, 104970, 131744, 630230]
+                            },
+                            {
+                                name: '2012年',
+                                type: 'bar',
+                                data: [19325, 23438, 31000, 121594, 134141, 681807]
+                            }
+                        ]
+                    }])
+                })
+            }))
+        })
+        //Line Chart
+        mock.onGet('/chart/line').reply(config => {
+            return new Promise(((resolve, reject) => {
+                setTimeout(() => {
+                    resolve([200, {
+                        title: {
+                            text: 'Line Chart'
+                        },
+                        tooltip: {
+                            trigger: 'axis'
+                        },
+                        legend: {
+                            data: ['邮件营销', '联盟广告', '搜索引擎']
+                        },
+                        grid: {
+                            left: '3%',
+                            right: '4%',
+                            bottom: '3%',
+                            containLabel: true
+                        },
+                        xAxis: {
+                            type: 'category',
+                            boundaryGap: false,
+                            data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+                        },
+                        yAxis: {
+                            type: 'value'
+                        },
+                        series: [
+                            {
+                                name: '邮件营销',
+                                type: 'line',
+                                stack: '总量',
+                                data: [120, 132, 101, 134, 90, 230, 210]
+                            },
+                            {
+                                name: '联盟广告',
+                                type: 'line',
+                                stack: '总量',
+                                data: [220, 182, 191, 234, 290, 330, 310]
+                            },
+                            {
+                                name: '搜索引擎',
+                                type: 'line',
+                                stack: '总量',
+                                data: [820, 932, 901, 934, 1290, 1330, 1320]
+                            }
+                        ]
+                    }])
+                })
+            }))
+        })
+        //Pie Chart
+        mock.onGet(`/chart/pie`).reply(config => {
+            return new Promise(((resolve, reject) => {
+                setTimeout(() => {
+                    resolve([200,{
+                        title: {
+                            text: 'Pie Chart',
+                            subtext: '纯属虚构',
+                            x: 'center'
+                        },
+                        tooltip: {
+                            trigger: 'item',
+                            formatter: "{a} <br/>{b} : {c} ({d}%)"
+                        },
+                        legend: {
+                            orient: 'vertical',
+                            left: 'left',
+                            data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
+                        },
+                        series: [
+                            {
+                                name: '访问来源',
+                                type: 'pie',
+                                radius: '55%',
+                                center: ['50%', '60%'],
+                                data: [
+                                    {value: 335, name: '直接访问'},
+                                    {value: 310, name: '邮件营销'},
+                                    {value: 234, name: '联盟广告'},
+                                    {value: 135, name: '视频广告'},
+                                    {value: 1548, name: '搜索引擎'}
+                                ],
+                                itemStyle: {
+                                    emphasis: {
+                                        shadowBlur: 10,
+                                        shadowOffsetX: 0,
+                                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                                    }
+                                }
+                            }
+                        ]
+                    }])
+                })
+            }))
+        })
 
-  }
+
+    }
 };
